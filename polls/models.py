a8080496eb5d7bf5064321bbac73f8697753cb87
Quentin Marceron <quentin.marceron@epsi.fr>
2023-04-04 21:44:13 +0200

    Draft

--- polls/models.py
+++ polls/models.py
@@ -7,5 +7,5 @@ from django.utils import timezone
 # Create your models here.
 class Ecole(models.Model):
     name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500, blank='true')
     location = models.CharField(max_length=200)
-
--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 # Create your views here.
-def login(request):
-    return render(request, 'connect/login.html')
+def signin(request):
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST['mail'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    else:
+        return render(request, 'registration/signin.html')
 
 
-def register(request):
-    return render(request, 'connect/register.html')
+def logout_view(request):
+    logout(request)
+    return render(request, 'registration/signin.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(request.POST['mail'], request.POST['mail'], request.POST['password'])
+        user.first_name = request.POST['firstname']
+        user.last_name = request.POST['lastname']
+        user.save()
+        return redirect('signin')
+    else:
+        return render(request, 'registration/signup.html')
 
 
 def home(request):
     return render(request, 'home/home.html')
+
+
+def book(request):
+    return render(request, 'book/book.html')
