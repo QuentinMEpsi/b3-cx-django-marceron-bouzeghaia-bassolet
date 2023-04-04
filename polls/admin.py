9b031ea33c1c45053c406e7fe550e9155509cae1
Quentin Marceron <quentin.marceron@epsi.fr>
2023-04-04 14:57:06 +0200

    Draft

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from polls.models import User
+from polls.models import Ecole
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(Ecole)
--- polls/models.py
+++ polls/models.py
@@ -5,14 +5,6 @@ from django.utils import timezone
 
 
 # Create your models here.
-class User(models.Model):
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    phone = models.CharField(max_length=50)
-    mail = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-
-
 class Ecole(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=200)
